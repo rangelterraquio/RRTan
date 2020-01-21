211381681af0a47a63a95cee66ed64321ee05f64
Rangel Dias <rterraquio@gmail.com>
2020-01-21 14:53:50 -0300

    First Commit

---
+++ RRTan/Utils.swift
@@ -0,0 +1,52 @@
+//
+//  Utils.swift
+//  RRTan
+//
+//  Created by Rangel Cardoso Dias on 21/01/20.
+//  Copyright © 2020 Rangel Cardoso Dias. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension CGFloat{
+    func degreesToradius() -> CGFloat {
+        return self * .pi / 180
+    }
+    func radiusToDegree() -> CGFloat {
+        return self  * 180 / .pi
+    }
+}
+
+//operações com CGPoint
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+  func sqrt(a: CGFloat) -> CGFloat {
+    return CGFloat(sqrtf(Float(a)))
+  }
+#endif
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
+  }
+  
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+}
